a3123ff79276a7d988071b164ba75539f954c60d
SpyrosPolemis <spy.polemis@gmail.com>
2026-06-23 02:29:39 +1000

    feat(Gameboard): add placeShip method

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -7,8 +7,58 @@ export class Gameboard {
         this.board[i][j] = { ship: null, hit: false };
       }
     }
-    console.log(this.board);
+  }
+
+  placeShip(ship, coords, orientation = "h") {
+    if (!this.#checkShipFits(ship, coords, orientation)) {
+      return false;
+    }
+
+    if (orientation === "h") {
+      for (let i = coords[0]; i < coords[0] + ship.length; i++) {
+        this.board[i][coords[1]].ship = ship;
+      }
+    }
+    if (orientation === "v") {
+      for (let i = coords[1]; i < coords[1] + ship.length; i++) {
+        this.board[coords[0]][i].ship = ship;
+      }
+    }
+
+    return true;
+  }
+
+  #checkShipFits(ship, coords, orientation) {
+    /* return false if ship will overlaps existing ship or if it will overflow from board
+    else return true */
+    if (coords[0] > 9 || coords[0] < 0 || coords[1] > 9 || coords[1] < 0) {
+      return false;
+    }
+    if ((orientation = "h")) {
+      if (coords[0] + ship.length > 9) {
+        return false;
+      }
+    }
+    if ((orientation = "v")) {
+      if (coords[1] + ship.length > 9) {
+        return false;
+      }
+    }
+
+    for (let i = coords[0]; i < coords[0] + ship.length; i++) {
+      console.log(i);
+      console.log(this.board[i][coords[1]]);
+      if (this.board[i][coords[1]].ship !== null) {
+        return false;
+      }
+    }
+
+    for (let i = coords[1]; i < coords[1] + ship.length; i++) {
+      if (this.board[coords[0]][i].ship !== null) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
-
-const game = new Gameboard();
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -1,13 +1,31 @@
 import { Gameboard } from "./Gameboard.js";
+import { Ship } from "./Ship.js";
 
 describe("Gameboard", () => {
   let gameboard;
 
   beforeEach(() => {
     gameboard = new Gameboard();
+    gameboard.placeShip(new Ship(3), [4, 4]);
   });
 
   it("creates gameboard", () => {
     expect(gameboard).toBeTruthy();
+  });
+
+  it("places horizontal ship correctly", () => {
+    expect(gameboard.placeShip(new Ship(3), [0, 1])).toBeTruthy();
+  });
+
+  it("places vertical ship correctly", () => {
+    expect(gameboard.placeShip(new Ship(3), [0, 1], "v")).toBeTruthy();
+  });
+
+  it("handles ship overlap", () => {
+    expect(gameboard.placeShip(new Ship(3), [3, 4])).toBeFalsy();
+  });
+
+  it("handles ship overflow", () => {
+    expect(gameboard.placeShip(new Ship(3), [7, 4])).toBeFalsy();
   });
 });
