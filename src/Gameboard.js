86f114b12a2f118b3985a79ce70a01b3469e6afe
SpyrosPolemis <spy.polemis@gmail.com>
2026-06-23 01:41:17 +1000

    feat(Gameboard): add constructor

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,3 +1,14 @@
 export class Gameboard {
-  constructor() {}
+  constructor() {
+    this.board = [];
+    for (let i = 0; i < 10; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < 10; j++) {
+        this.board[i][j] = { ship: null, hit: false };
+      }
+    }
+    console.log(this.board);
+  }
 }
+
+const game = new Gameboard();
