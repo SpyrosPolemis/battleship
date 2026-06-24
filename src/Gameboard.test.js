9a17045821e339b4dfbe4ae6737842f72e4ae35e
SpyrosPolemis <spy.polemis@gmail.com>
2026-06-24 18:12:35 +1000

    test(Gameboard): test placed ship is in correct squares

--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -14,11 +14,19 @@ describe("Gameboard", () => {
   });
 
   it("places horizontal ship correctly", () => {
-    expect(gameboard.placeShip(new Ship(3), [0, 1])).toBeTruthy();
+    const ship = new Ship(3);
+    expect(gameboard.placeShip(ship, [0, 1])).toBeTruthy();
+    expect(gameboard.board[0][1].ship).toBe(ship);
+    expect(gameboard.board[1][1].ship).toBe(ship);
+    expect(gameboard.board[2][1].ship).toBe(ship);
   });
 
   it("places vertical ship correctly", () => {
-    expect(gameboard.placeShip(new Ship(3), [0, 1], "v")).toBeTruthy();
+    const ship = new Ship(3);
+    expect(gameboard.placeShip(ship, [0, 1], "v")).toBeTruthy();
+    expect(gameboard.board[0][1].ship).toBe(ship);
+    expect(gameboard.board[0][2].ship).toBe(ship);
+    expect(gameboard.board[0][3].ship).toBe(ship);
   });
 
   it("handles ship overlap", () => {
